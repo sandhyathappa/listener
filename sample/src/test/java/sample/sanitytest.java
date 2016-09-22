100e525a8c53f1d6ea760da9d6bb85b9df487950
sandhyathappa <https://github.com/sandhyathappa/project.git>
2016-09-22 19:58:20 +0530

    listeners

---
+++ sample/src/test/java/sample/sanitytest.java
@@ -0,0 +1,43 @@
+package sample;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import atu.testng.reports.listeners.ATUReportsListener;
+import atu.testng.reports.listeners.ConfigurationListener;
+import atu.testng.reports.listeners.MethodListener;
+
+
+
+@Listeners({ATUReportsListener.class,ConfigurationListener.class,MethodListener.class })
+public class sanitytest 
+{
+	WebDriver driver;
+	{
+		System.setProperty("atu.report.config", "C:\\eclipse proram\\atu.properties");
+	}
+	@Test
+	@Parameters({"browser"})
+public void sanitytest(String br) throws Exception
+  {
+   if(br.matches("Chrome"))
+   {
+	   System.setProperty("webdriver.chrome.driver","C:\\chromedriver.exe" );
+	   driver=new ChromeDriver();
+   }
+
+   home h=new home(driver);
+   h.login();
+   
+   compose c=new compose(driver);
+   c.sendmail();
+   c.signout();
+   
+   
+   }
+
+
+}
